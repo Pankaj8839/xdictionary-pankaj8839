a01687dd304ebcfd2697844d10b40df1f8824150
Pankaj8839 <pankajrajput8839@gmail.com.>
2024-12-07 18:51:45 +0530

    add file

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [word, setWord] = useState('');
+  const [meaning, setMeaning] = useState('');
+  const Dictionary =
+  [
+  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  
+      { word: "Component", meaning: "A reusable building block in React." },
+  
+      { word: "State", meaning: "An object that stores data for a component." }
+  
+  ]
+  
+  const handleClick = () => {
+    let mean = "";
+    Dictionary.forEach((item) => {
+      if (item.word === word) {
+        mean= item.meaning;
+      }
+    });
+    mean= mean === "" ? "Word not found in the dictionary." : mean;
+    setMeaning(mean);
+  }
+  
+  return (
+    <div className="App">
+     <h1>Dictionary App</h1>
+     <input type="text" value={word} onChange={(e) => setWord(e.target.value)} placeholder='Search for a word...'/>
+     <button onClick={handleClick}>Search</button>
+     <h5>Definition:</h5>
+     <p>{meaning}</p>
+    </div>
+  );
+}
+
+export default App;
